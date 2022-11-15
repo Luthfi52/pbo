1202894f424a4d7d78b7ec09cbe79652e9657a47
Luthfi52 <114200621+Luthfi52@users.noreply.github.com>
2022-11-16 00:48:48 +0700

    tugas github

---
+++ src/luthfi_120922/DataDiri.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luthfi_120922;
+
+/**
+ *
+ * @author ACER
+ */
+public class DataDiri {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Luthfi Firstian");
+        System.out.println("No BP   : 2001092052");
+        System.out.println("Prodi   : D3 Manajemen Informatika");
+        System.out.println("Jurusan : Teknlogi Informasi");
+        System.out.println("Alamat  : Sawahan");
+
+    }
+
+}
